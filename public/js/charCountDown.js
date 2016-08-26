e30f890819085c94d6cdf1dc70f12f4900e7a185
nialloc9@gmail.com <nialloc9@gmail.com>
2016-08-26 16:50:36 +0900

    Updated for reuseability

---
+++ public/js/charCountDown.js
@@ -0,0 +1,11 @@
+function updateCountdown(max, userInput, extendedMessage, countdownAreaClass) {
+    //ASSIGN VARIABLES
+    var ui = '.' + userInput;
+    var area = '.' + countdownAreaClass;
+
+    var remaining = max - $(ui).val().length;
+
+    //ADD REMAINING
+    $(area).text(remaining + extendedMessage);
+    console.log(remaining);
+}
